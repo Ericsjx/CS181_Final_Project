c22abbed3fdfacc5580d6b1d2fe8ffd2ba46f809
Jason-Windows-Desktop <s1506479863@163.com>
2024-06-21 19:33:01 +0800

    update main

--- main.py
+++ main.py
@@ -14,13 +14,12 @@ if __name__ == '__main__':
     agent1 = RandomAI(Reversi.BLACK)
     #agent1 = MCTSAgent(Reversi.BLACK, simulations=1000)  # MCTS代理
     #agent1 = ApproximateQLearningAI(Reversi.BLACK)
-    #agent1 =QLearningAI(Reversi.BLACK)
+    #agent1 = QLearningAI(Reversi.BLACK)
     #agent1 = MinimaxAI(Reversi.BLACK,depth=3)
     #agent2 = RandomAI(Reversi.WHITE)  # Minimax代理
-    agent2=GreedyAI(Reversi.WHITE)
+    agent2 = GreedyAI(Reversi.WHITE)
     #agent2 = MinimaxAI(Reversi.WHITE,depth=3)
     #agent2 = MCTSAgent(Reversi.WHITE, simulations=1000)  # MCTS代理
-    #train_agent_ApproximateQLearning(episodes=4000,agent=agent1,opponent=RandomAI(Reversi.WHITE),game_class=Reversi)
     parser = argparse.ArgumentParser(description="Reversi AI")
     parser.add_argument("--no-GUI", action="store_true", help="Run tests without GUI")
     args = parser.parse_args()
